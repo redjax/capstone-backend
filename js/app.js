80906ddbce000a754357d8015b97ed20c12b74e4
Jack Kenyon <jackenyon@gmail.com>
2018-04-21 20:58:58 -0400

    Oberlin2
    Tweaked Oberlin2, made a little progress.

--- js/app.js
+++ js/app.js
@@ -1,53 +1,54 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-// Page Piling Initialize
+        // Page Piling Initialize
 
-	$('#pagepiling').pagepiling({
-	    menu: null,
-        direction: 'horizontal',
-        horizontalCentered: true,
-        sectionsColor: [],
-        anchors: [],
-        scrollingSpeed: 100,
-        easing: 'swing',
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        navigation: {
-            'textColor': '#fff',
-            'bulletsColor': '#fff',
-            'position': 'bottom-center',
-            'tooltips': ['section1', 'section2', 'section3']
-        },
-       	normalScrollElements: null,
-        normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 2,
-        keyboardScrolling: true,
-        sectionSelector: '.section',
-        animateAnchor: false,
+        $('#pagepiling').pagepiling({
+                menu: null,
+                direction: 'horizontal',
+                verticalCentered: true,
+                horizontalCentered: true,
+                sectionsColor: [],
+                anchors: [],
+                scrollingSpeed: 100,
+                easing: 'swing',
+                loopBottom: false,
+                loopTop: false,
+                css3: true,
+                navigation: {
+                        'textColor': '#fff',
+                        'bulletsColor': '#fff',
+                        'position': 'bottom-center',
+                        'tooltips': ['section1', 'section2', 'section3']
+                },
+                normalScrollElements: null,
+                normalScrollElementTouchThreshold: 5,
+                touchSensitivity: 2,
+                keyboardScrolling: true,
+                sectionSelector: '.section',
+                animateAnchor: false,
 
         });
 
 
-// Menu Toggling
+        // Menu Toggling
 
-        $('.burger, .overlay').click(function(){
-        $('.burger').toggleClass('clicked');
-        $('.overlay').toggleClass('show');
-        $('nav').toggleClass('show');
-        $('body').toggleClass('overflow');
+        $('.burger, .overlay').click(function () {
+                $('.burger').toggleClass('clicked');
+                $('.overlay').toggleClass('show');
+                $('nav').toggleClass('show');
+                $('body').toggleClass('overflow');
         });
 
 
-// iZiModals
+        // iZiModals
 
 
-//IZI Modal [START]
+        //IZI Modal [START]
         $("#page1-expand").iziModal({
-        overlayClose: false,
-        overlayColor: 'rgba(0, 0, 0, 0.6)'
+                overlayClose: false,
+                overlayColor: 'rgba(0, 0, 0, 0.6)'
 
-});
+        });
 
 
 });
